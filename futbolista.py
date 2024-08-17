e8eef811005970a14127053a75aa9ef27c8df055
Johan Ramirez Marin <159493340+joramirezma@users.noreply.github.com>
2024-08-17 11:16:31 -0500

    Second commit

--- futbolista.py
+++ futbolista.py
@@ -4,9 +4,9 @@ from persona import Persona
 class Futbolista(Persona,Deportista):
     listaFutbolistas = []
 
-    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
         Persona.__init__(nombre, edad, altura, sexo)
-        Deportista.__init__(deporte,añosPracticando)
+        Deportista.__init__(añosPracticando)
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
